c89f8ad45aa746fd9b2020fa1bf8e6a35aceaae6
Anthony Pesce <anthonyjpesce@gmail.com>
2016-09-24 10:18:30 -0700

    more setup

---
+++ clinics/urls.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.contrib import admin
+from toolbox import views as toolbox_views
+from django.conf.urls import include, url
+from django.views.static import serve as static_serve
+
+
+urlpatterns = [
+
+]
