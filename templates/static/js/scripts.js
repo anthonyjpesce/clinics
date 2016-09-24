5859fd5a43dfd4be94814fd2c7b6eac40311da9d
Jon Schleuss <gaufre@gmail.com>
2016-09-24 16:07:39 -0700

    feels slicker

--- templates/static/js/scripts.js
+++ templates/static/js/scripts.js
@@ -1,31 +1,37 @@
 /* Copyright Anthony J. Pesce and Jon Schleuss */
 
 document.getElementById('find_locations').onclick=function(){
-	console.log('find location');
-	getLocation(); // get users location
+    console.log('find location');
+    getLocation(); // get users location
+
+    loadClinics(); // will be moved
 };
 
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-    	console.log("Geolocation is not supported by this browser.");
+        console.log("Geolocation is not supported by this browser.");
     }
 }
 function showPosition(position) {
-	console.log(position.coords.latitude + "," + position.coords.longitude);
+    console.log(position.coords.latitude + "," + position.coords.longitude);
 }
 
 // get list of clinics that match
-$.getJSON( "/static/js/90029-50.json", function( data ) {
-	console.log(data);
-  var items = [];
-  $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
-  });
- 
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
-});
+function loadClinics() {
+    // hide init header and button
+    $("#ini-content").fadeOut(400, function(){
+        // after init content disappears
+        $(".navbar").fadeIn(400);
+    });
+
+    // alter top of page for search results
+    // add filter options
+
+    // create list
+    $.getJSON( "/static/js/90029-50-more-data.json", function( data ) {
+        console.log(data);
+    });
+}
+
