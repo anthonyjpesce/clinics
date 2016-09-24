e31f126a6253f4816b2ca62b5d8be8eb33d45ec1
Anthony Pesce <anthonyjpesce@gmail.com>
2016-09-24 11:13:18 -0700

    django 1.10 changes and some fabfile adds

--- clinics/urls.py
+++ clinics/urls.py
@@ -1,10 +1,8 @@
+from clinics import views
 from django.conf import settings
-from django.contrib import admin
-from toolbox import views as toolbox_views
 from django.conf.urls import include, url
-from django.views.static import serve as static_serve
 
 
 urlpatterns = [
-
+    url(r'^$', views.IndexView.as_view()),
 ]
