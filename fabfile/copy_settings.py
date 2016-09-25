b109956d35f64e1c75c9bea545bd0324ce8bd068
Anthony Pesce <anthonyjpesce@gmail.com>
2016-09-24 23:06:48 -0700

    fabric

---
+++ fabfile/copy_settings.py
@@ -0,0 +1,7 @@
+from fabric.operations import put
+from fabric.api import env, task
+
+@task
+def copy_settings():
+    put('/home/anthony/code/personal/clinics/repo/project/settings_private.py', '/apps/clinics/repo/project/settings_private.py')
+
